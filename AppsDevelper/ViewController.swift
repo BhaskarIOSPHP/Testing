262fa9b9dbb442937f92b8f4cc706e1f1f62a7d0
Udaya Bhaskar <parella.udayabhaskar@gmail.com>
2019-03-14 16:56:06 +0530

    Add The Print statement in View Controller

--- AppsDevelper/ViewController.swift
+++ AppsDevelper/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        print("This is View Controller")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
